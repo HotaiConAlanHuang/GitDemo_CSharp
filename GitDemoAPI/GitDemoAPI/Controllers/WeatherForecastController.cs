b78a005a15d7d8a49f0ba081585dd66f9c7691d3
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-01-15 14:09:23 +0800

    feat: add "xxxx"

--- GitDemoAPI/GitDemoAPI/Controllers/WeatherForecastController.cs
+++ GitDemoAPI/GitDemoAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitDemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","xxxx"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;

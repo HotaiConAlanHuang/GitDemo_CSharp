0c380701507f8bb0d569379e63789b4cad48cd56
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-01-16 11:22:53 +0800

    Rebase 練習功能

--- GitDemoAPI/GitDemoAPI/Controllers/WeatherForecastController.cs
+++ GitDemoAPI/GitDemoAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GitDemoAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","xxxx","xxx2"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","xxxx"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;

8eed7be2a4c1552a4b94f8af86284e48b49ebe8d
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-01-16 10:48:35 +0800

    增加一個變數

--- GitDemoAPI/GitDemoAPI/WeatherForecast.cs
+++ GitDemoAPI/GitDemoAPI/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace GitDemoAPI
         public int? TemperatureF =>  31 + (int)(TemperatureC / 0.5556);
       
         public string? Summary { get; set; }
+        public string? NewField { get; set; }
     }
 }
